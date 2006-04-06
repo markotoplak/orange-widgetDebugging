c3a0606b03e42db069f276111e99ec9e96f265a9
tomazc <tomazc@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2006-04-06 07:22:52 +0000

    no message

---
+++ updateStart.bat
@@ -0,0 +1,10 @@
+c:
+cd \Python23\Lib\site-packages
+rem cvs -d :sspi:cvs@estelle.fri.uni-lj.si:/CVS checkout -A -- orange
+rem cvs -d :sspi:cvs@estelle.fri.uni-lj.si:/CVS checkout -- WidgetDebugging
+
+cd orange
+cvs update -d -C
+cd ..\widgetDebugging
+cvs update -d -C
+python debugWidgets.py
