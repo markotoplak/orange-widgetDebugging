99e5ca8758216674084b13321720b1d30d0dcdad
Gregor <Gregor@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2007-04-04 09:48:12 +0000

    - more complex schema

--- visualizations.py
+++ visualizations.py
@@ -2,28 +2,18 @@
 # datasets: class, noclass
 # ignore: titanic.tab
 
-import sys, os, cPickle, orange
-import orngSignalManager
-
-#set value in next line to 1 if want to output debugging info to file 'signalManagerOutput.txt'
-DEBUG_MODE = 0
-
-widgetDir = os.path.join(os.path.split(orange.__file__)[0], "OrangeWidgets")
-if os.path.exists(widgetDir):
-        for name in os.listdir(widgetDir):
-            fullName = os.path.join(widgetDir, name)
-            if os.path.isdir(fullName): sys.path.append(fullName)
+import sys, os, cPickle, orange, orngSignalManager, orngRegistry
+DEBUG_MODE = 0   #set to 1 to output debugging info to file 'signalManagerOutput.txt'
+orngRegistry.addWidgetDirectories()
 
 from OWFile import *
 from OWDataSampler import *
-from OWAttributeStatistics import *
+from OWDataDomain import *
 from OWScatterPlot import *
 from OWLinProj import *
 from OWRadviz import *
 from OWPolyviz import *
 from OWParallelCoordinates import *
-from OWSurveyPlot import *
-from OWSieveDiagram import *
 from OWMosaicDisplay import *
 
 
@@ -37,16 +27,14 @@ class GUIApplication(QVBox):
         # create widget instances
         self.owFile = OWFile(signalManager = self.signalManager)
         self.owData_Sampler = OWDataSampler(signalManager = self.signalManager)
-        self.owAttribute_Statistics = OWAttributeStatistics(signalManager = self.signalManager)
+        self.owSelect_Attributes = OWDataDomain(signalManager = self.signalManager)
         self.owScatterplot = OWScatterPlot(signalManager = self.signalManager)
         self.owLinear_Projection = OWLinProj(signalManager = self.signalManager)
         self.owRadviz = OWRadviz(signalManager = self.signalManager)
         self.owPolyviz = OWPolyviz(signalManager = self.signalManager)
         self.owParallel_coordinates = OWParallelCoordinates(signalManager = self.signalManager)
-        self.owSurvey_Plot = OWSurveyPlot(signalManager = self.signalManager)
-        self.owSieve_Diagram = OWSieveDiagram(signalManager = self.signalManager)
         self.owMosaic_Display = OWMosaicDisplay(signalManager = self.signalManager)
-        
+
         # create instances of hidden widgets
 
         #set event and progress handler
@@ -54,8 +42,8 @@ class GUIApplication(QVBox):
         self.owFile.setProgressBarHandler(self.progressHandler)
         self.owData_Sampler.setEventHandler(self.eventHandler)
         self.owData_Sampler.setProgressBarHandler(self.progressHandler)
-        self.owAttribute_Statistics.setEventHandler(self.eventHandler)
-        self.owAttribute_Statistics.setProgressBarHandler(self.progressHandler)
+        self.owSelect_Attributes.setEventHandler(self.eventHandler)
+        self.owSelect_Attributes.setProgressBarHandler(self.progressHandler)
         self.owScatterplot.setEventHandler(self.eventHandler)
         self.owScatterplot.setProgressBarHandler(self.progressHandler)
         self.owLinear_Projection.setEventHandler(self.eventHandler)
@@ -66,19 +54,10 @@ class GUIApplication(QVBox):
         self.owPolyviz.setProgressBarHandler(self.progressHandler)
         self.owParallel_coordinates.setEventHandler(self.eventHandler)
         self.owParallel_coordinates.setProgressBarHandler(self.progressHandler)
-        self.owSurvey_Plot.setEventHandler(self.eventHandler)
-        self.owSurvey_Plot.setProgressBarHandler(self.progressHandler)
-        self.owSieve_Diagram.setEventHandler(self.eventHandler)
-        self.owSieve_Diagram.setProgressBarHandler(self.progressHandler)
         self.owMosaic_Display.setEventHandler(self.eventHandler)
         self.owMosaic_Display.setProgressBarHandler(self.progressHandler)
-        
 
         #list of widget instances
-        self.widgets = [self.owFile, self.owData_Sampler,
-                        self.owAttribute_Statistics, self.owScatterplot, self.owLinear_Projection, self.owRadviz, self.owPolyviz,
-                        self.owParallel_coordinates, self.owSurvey_Plot, self.owSieve_Diagram, self.owMosaic_Display]
-        
         vizranks = [self.owScatterplot.vizrank, self.owLinear_Projection.vizrank, self.owRadviz.vizrank]        # don't add polyviz's vizrank, because it doesn't use time limit!!
         for w in vizranks:
             w.useTimeLimit = 1
@@ -89,71 +68,62 @@ class GUIApplication(QVBox):
             w.optimizeProjectionLimit = 20
             w.attributeCount = 6
             w.setEventHandler(self.eventHandler)
-        
+
+        self.widgets = [self.owFile, self.owData_Sampler, self.owSelect_Attributes, self.owScatterplot,
+                        self.owLinear_Projection, self.owRadviz, self.owPolyviz, self.owParallel_coordinates,
+                        self.owMosaic_Display, ]
         self.widgets += vizranks
-        
+
         # set widget captions
         self.owFile.setCaptionTitle('Qt File')
         self.owData_Sampler.setCaptionTitle('Qt Data Sampler')
-        self.owAttribute_Statistics.setCaptionTitle('Qt Attribute Statistics')
+        self.owSelect_Attributes.setCaptionTitle('Qt Select Attributes')
         self.owScatterplot.setCaptionTitle('Qt Scatterplot')
         self.owLinear_Projection.setCaptionTitle('Qt Linear Projection')
         self.owRadviz.setCaptionTitle('Qt Radviz')
         self.owPolyviz.setCaptionTitle('Qt Polyviz')
         self.owParallel_coordinates.setCaptionTitle('Qt Parallel coordinates')
-        self.owSurvey_Plot.setCaptionTitle('Qt Survey Plot')
-        self.owSieve_Diagram.setCaptionTitle('Qt Sieve Diagram')
         self.owMosaic_Display.setCaptionTitle('Qt Mosaic Display')
-        
+
         # set icons
         self.owFile.setWidgetIcon('icons/File.png')
         self.owData_Sampler.setWidgetIcon('icons/DataSampler.png')
-        self.owAttribute_Statistics.setWidgetIcon('icons/AttributeStatistics.png')
+        self.owSelect_Attributes.setWidgetIcon('icons/SelectAttributes.png')
         self.owScatterplot.setWidgetIcon('icons/ScatterPlot.png')
-        self.owLinear_Projection.setWidgetIcon('icons/LinProj.png')
+        self.owLinear_Projection.setWidgetIcon('icons/LinearProjection.png')
         self.owRadviz.setWidgetIcon('icons/Radviz.png')
         self.owPolyviz.setWidgetIcon('icons/Polyviz.png')
         self.owParallel_coordinates.setWidgetIcon('icons/ParallelCoordinates.png')
-        self.owSurvey_Plot.setWidgetIcon('icons/SurveyPlot.png')
-        self.owSieve_Diagram.setWidgetIcon('icons/SieveDiagram.png')
         self.owMosaic_Display.setWidgetIcon('icons/MosaicDisplay.png')
-        
+
         self.signalManager.addWidget(self.owFile)
         self.signalManager.addWidget(self.owData_Sampler)
-        self.signalManager.addWidget(self.owAttribute_Statistics)
+        self.signalManager.addWidget(self.owSelect_Attributes)
         self.signalManager.addWidget(self.owScatterplot)
         self.signalManager.addWidget(self.owLinear_Projection)
         self.signalManager.addWidget(self.owRadviz)
         self.signalManager.addWidget(self.owPolyviz)
         self.signalManager.addWidget(self.owParallel_coordinates)
-        self.signalManager.addWidget(self.owSurvey_Plot)
-        self.signalManager.addWidget(self.owSieve_Diagram)
         self.signalManager.addWidget(self.owMosaic_Display)
-        
+
         # create widget buttons
         owButtonFile = QPushButton("File", self)
-        frameSpace1 = QFrame(self);  frameSpace1.setMinimumHeight(10); frameSpace1.setMaximumHeight(10)
         owButtonData_Sampler = QPushButton("Data Sampler", self)
-        frameSpace2 = QFrame(self);  frameSpace2.setMinimumHeight(10); frameSpace2.setMaximumHeight(10)
-        owButtonAttribute_Statistics = QPushButton("Attribute Statistics", self)
+        owButtonSelect_Attributes = QPushButton("Select Attributes", self)
         owButtonScatterplot = QPushButton("Scatterplot", self)
         owButtonLinear_Projection = QPushButton("Linear Projection", self)
         owButtonRadviz = QPushButton("Radviz", self)
         owButtonPolyviz = QPushButton("Polyviz", self)
         owButtonParallel_coordinates = QPushButton("Parallel coordinates", self)
-        owButtonSurvey_Plot = QPushButton("Survey Plot", self)
-        frameSpace3 = QFrame(self);  frameSpace3.setMinimumHeight(10); frameSpace3.setMaximumHeight(10)
-        owButtonSieve_Diagram = QPushButton("Sieve Diagram", self)
         owButtonMosaic_Display = QPushButton("Mosaic Display", self)
         frameSpace = QFrame(self);  frameSpace.setMinimumHeight(20); frameSpace.setMaximumHeight(20)
         exitButton = QPushButton("E&xit",self)
         self.connect(exitButton,SIGNAL("clicked()"), application, SLOT("quit()"))
-        
+
 
         statusBar = QStatusBar(self)
         self.progress = QProgressBar(100, statusBar)
-        self.progress.setMaximumWidth(80)
-        self.progress.setMinimumWidth(80)
+        self.progress.setMaximumWidth(100)
         self.progress.setCenterIndicator(1)
         self.status = QLabel("", statusBar)
         self.status.setSizePolicy(QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred))
@@ -162,43 +132,41 @@ class GUIApplication(QVBox):
         #connect GUI buttons to show widgets
         self.connect(owButtonFile ,SIGNAL("clicked()"), self.owFile.reshow)
         self.connect(owButtonData_Sampler ,SIGNAL("clicked()"), self.owData_Sampler.reshow)
-        self.connect(owButtonAttribute_Statistics ,SIGNAL("clicked()"), self.owAttribute_Statistics.reshow)
+        self.connect(owButtonSelect_Attributes ,SIGNAL("clicked()"), self.owSelect_Attributes.reshow)
         self.connect(owButtonScatterplot ,SIGNAL("clicked()"), self.owScatterplot.reshow)
         self.connect(owButtonLinear_Projection ,SIGNAL("clicked()"), self.owLinear_Projection.reshow)
         self.connect(owButtonRadviz ,SIGNAL("clicked()"), self.owRadviz.reshow)
         self.connect(owButtonPolyviz ,SIGNAL("clicked()"), self.owPolyviz.reshow)
         self.connect(owButtonParallel_coordinates ,SIGNAL("clicked()"), self.owParallel_coordinates.reshow)
-        self.connect(owButtonSurvey_Plot ,SIGNAL("clicked()"), self.owSurvey_Plot.reshow)
-        self.connect(owButtonSieve_Diagram ,SIGNAL("clicked()"), self.owSieve_Diagram.reshow)
         self.connect(owButtonMosaic_Display ,SIGNAL("clicked()"), self.owMosaic_Display.reshow)
-        
+
         #load settings before we connect widgets
         self.loadSettings()
 
         # add widget signals
         self.signalManager.setFreeze(1)
         self.signalManager.addLink( self.owFile, self.owData_Sampler, 'Examples', 'Data', 1)
-        self.signalManager.addLink( self.owFile, self.owAttribute_Statistics, 'Examples', 'Examples', 1)
-        self.signalManager.addLink( self.owFile, self.owScatterplot, 'Examples', 'Examples', 1)
-        self.signalManager.addLink( self.owFile, self.owLinear_Projection, 'Classified Examples', 'Examples', 1)
-        self.signalManager.addLink( self.owFile, self.owRadviz, 'Classified Examples', 'Examples', 1)
-        self.signalManager.addLink( self.owFile, self.owParallel_coordinates, 'Examples', 'Examples', 1)
-        self.signalManager.addLink( self.owFile, self.owSurvey_Plot, 'Examples', 'Examples', 1)
-        self.signalManager.addLink( self.owFile, self.owSieve_Diagram, 'Examples', 'Examples', 1)
-        self.signalManager.addLink( self.owFile, self.owMosaic_Display, 'Examples', 'Examples', 1)
-        self.signalManager.addLink( self.owData_Sampler, self.owScatterplot, 'Classified Examples', 'Example Subset', 1)
-        self.signalManager.addLink( self.owData_Sampler, self.owLinear_Projection, 'Classified Examples', 'Example Subset', 1)
-        self.signalManager.addLink( self.owData_Sampler, self.owRadviz, 'Classified Examples', 'Example Subset', 1)
-        self.signalManager.addLink( self.owFile, self.owPolyviz, 'Classified Examples', 'Examples', 1)
-        self.signalManager.addLink( self.owData_Sampler, self.owParallel_coordinates, 'Classified Examples', 'Example Subset', 1)
-        self.signalManager.addLink( self.owData_Sampler, self.owMosaic_Display, 'Classified Examples', 'Example Subset', 1)
+        self.signalManager.addLink( self.owData_Sampler, self.owScatterplot, 'Examples', 'Examples', 1)
+        self.signalManager.addLink( self.owData_Sampler, self.owLinear_Projection, 'Examples', 'Examples', 1)
+        self.signalManager.addLink( self.owData_Sampler, self.owRadviz, 'Examples', 'Examples', 1)
+        self.signalManager.addLink( self.owData_Sampler, self.owPolyviz, 'Examples', 'Examples', 1)
+        self.signalManager.addLink( self.owData_Sampler, self.owParallel_coordinates, 'Examples', 'Examples', 1)
+        self.signalManager.addLink( self.owData_Sampler, self.owSelect_Attributes, 'Examples', 'Examples', 1)
+        self.signalManager.addLink( self.owSelect_Attributes, self.owScatterplot, 'Examples', 'Example Subset', 1)
+        self.signalManager.addLink( self.owSelect_Attributes, self.owLinear_Projection, 'Examples', 'Example Subset', 1)
+        self.signalManager.addLink( self.owSelect_Attributes, self.owPolyviz, 'Examples', 'Example Subset', 1)
+        self.signalManager.addLink( self.owSelect_Attributes, self.owRadviz, 'Examples', 'Example Subset', 1)
+        self.signalManager.addLink( self.owSelect_Attributes, self.owParallel_coordinates, 'Examples', 'Example Subset', 1)
+        self.signalManager.addLink( self.owData_Sampler, self.owMosaic_Display, 'Examples', 'Examples', 1)
+        self.signalManager.addLink( self.owSelect_Attributes, self.owMosaic_Display, 'Examples', 'Example Subset', 1)
         self.signalManager.setFreeze(0)
-        
+
+
 
     def eventHandler(self, text, eventVerbosity = 1):
         if self.verbosity >= eventVerbosity:
             self.status.setText(text)
-        
+
     def progressHandler(self, widget, val):
         if val < 0:
             self.status.setText("<nobr>Processing: <b>" + str(widget.captionTitle) + "</b></nobr>")
@@ -210,7 +178,8 @@ class GUIApplication(QVBox):
             self.progress.setProgress(val)
             self.update()
 
-        
+
+
     def loadSettings(self):
         try:
             file = open("visualizations.sav", "r")
@@ -222,8 +191,8 @@ class GUIApplication(QVBox):
         self.owFile.activateLoadedSettings()
         self.owData_Sampler.loadSettingsStr(strSettings["Data Sampler"])
         self.owData_Sampler.activateLoadedSettings()
-        self.owAttribute_Statistics.loadSettingsStr(strSettings["Attribute Statistics"])
-        self.owAttribute_Statistics.activateLoadedSettings()
+        self.owSelect_Attributes.loadSettingsStr(strSettings["Select Attributes"])
+        self.owSelect_Attributes.activateLoadedSettings()
         self.owScatterplot.loadSettingsStr(strSettings["Scatterplot"])
         self.owScatterplot.activateLoadedSettings()
         self.owLinear_Projection.loadSettingsStr(strSettings["Linear Projection"])
@@ -234,51 +203,41 @@ class GUIApplication(QVBox):
         self.owPolyviz.activateLoadedSettings()
         self.owParallel_coordinates.loadSettingsStr(strSettings["Parallel coordinates"])
         self.owParallel_coordinates.activateLoadedSettings()
-        self.owSurvey_Plot.loadSettingsStr(strSettings["Survey Plot"])
-        self.owSurvey_Plot.activateLoadedSettings()
-        self.owSieve_Diagram.loadSettingsStr(strSettings["Sieve Diagram"])
-        self.owSieve_Diagram.activateLoadedSettings()
         self.owMosaic_Display.loadSettingsStr(strSettings["Mosaic Display"])
         self.owMosaic_Display.activateLoadedSettings()
-        
-        
+
+
     def saveSettings(self):
         return
-        """
         if DEBUG_MODE: return
         self.owMosaic_Display.synchronizeContexts()
-        self.owSieve_Diagram.synchronizeContexts()
-        self.owSurvey_Plot.synchronizeContexts()
         self.owParallel_coordinates.synchronizeContexts()
         self.owPolyviz.synchronizeContexts()
         self.owRadviz.synchronizeContexts()
         self.owLinear_Projection.synchronizeContexts()
         self.owScatterplot.synchronizeContexts()
-        self.owAttribute_Statistics.synchronizeContexts()
+        self.owSelect_Attributes.synchronizeContexts()
         self.owData_Sampler.synchronizeContexts()
         self.owFile.synchronizeContexts()
-        
+
         strSettings = {}
         strSettings["File"] = self.owFile.saveSettingsStr()
         strSettings["Data Sampler"] = self.owData_Sampler.saveSettingsStr()
-        strSettings["Attribute Statistics"] = self.owAttribute_Statistics.saveSettingsStr()
+        strSettings["Select Attributes"] = self.owSelect_Attributes.saveSettingsStr()
         strSettings["Scatterplot"] = self.owScatterplot.saveSettingsStr()
         strSettings["Linear Projection"] = self.owLinear_Projection.saveSettingsStr()
         strSettings["Radviz"] = self.owRadviz.saveSettingsStr()
         strSettings["Polyviz"] = self.owPolyviz.saveSettingsStr()
         strSettings["Parallel coordinates"] = self.owParallel_coordinates.saveSettingsStr()
-        strSettings["Survey Plot"] = self.owSurvey_Plot.saveSettingsStr()
-        strSettings["Sieve Diagram"] = self.owSieve_Diagram.saveSettingsStr()
         strSettings["Mosaic Display"] = self.owMosaic_Display.saveSettingsStr()
-        
+
         file = open("visualizations.sav", "w")
         cPickle.dump(strSettings, file)
         file.close()
-        """
-        
 
 
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
     application = QApplication(sys.argv)
     ow = GUIApplication()
     application.setMainWidget(ow)
